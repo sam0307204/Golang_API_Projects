09829801f64e85ca658661b5fbc6a10173e57b73
Dhanush <119780513+sam0307204@users.noreply.github.com>
2023-11-09 07:15:56 +0530

    Update config.go

--- SignUp_API/server/DB_conect/config.go
+++ SignUp_API/server/DB_conect/config.go
@@ -9,7 +9,7 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:&ifconfig2309!_69*@tcp(127.0.0.1:3306)/signup?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "<username>:<password>@tcp(127.0.0.1:3306)/signup?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		log.Fatal(err)
